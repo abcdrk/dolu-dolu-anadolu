64d1409b183f7e9407ab6cc97016b63ee13ae4fb
Özge Dinçsoy <ozgedincsoy@Ozges-MacBook-Pro.local>
2019-05-25 13:04:16 +0300

    parToSentences.pickle is added by using helper.py

--- helper.py
+++ helper.py
@@ -1,21 +1,23 @@
 import nltk
 import pickle
 
-sent_detector = nltk.data.load('tokenizers/punkt/turkish.pickle')
+from nltk.tokenize import sent_tokenize
 
-f = open("derlem.txt", errors='ignore')
+sent_detec = nltk.data.load('tokenizers/punkt/turkish.pickle')
+
+f = open("derlem.txt", encoding="utf-16", errors='ignore')
 corpus = f.read()
-par_list = corpus.split("\n")
+par_list = corpus.split("\n\n")
 
 par_dict = {}
 
 for par in par_list:
+    print(par)
     start = par.index(" ")
     num = par[:start]
-    key = int(num, 10)
-    par_dict[key] = { "paragraph" : par, "sentences" : sent_detector(par[start+1:])}
-    break
+    key = int(num)
+    par_dict[key] = { "paragraph" : par[start+1:], "sentences" : sent_detec.tokenize(par[start+1:])}
 
-print(par_dict)
+print(len(par_dict))
 with open('parToSentences.pickle', 'wb') as handle:
     pickle.dump(par_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
